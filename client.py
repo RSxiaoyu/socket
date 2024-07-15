0b141bfe0cb15df57031942c1b4aaaa8650a5255
_xiaoyu <2872127712@qq.com>
2024-07-16 00:30:48 +0800

    remove test

--- client.py
+++ client.py
@@ -1,24 +1,20 @@
-import api
-local_ip=api.get_ip()
-print(local_ip)
-server=api.socket_server(local_ip)
-conn,address=server.accept()
-print(f"{address} > {local_ip} established.")
+from api import *
 
-target_address='192.168.2.121'
-client=api.socket_client(target_address)
-print(f"{local_ip} > {target_address} established.")
 
 def recvMethod():
     while True:
-        data=api.recv(conn)
+        data=recv(client)
         if data is not None:
             print(f'{data}')
 
 
 def inputMethod():
     while True:
-        api.send(client, input())
+        send(client, input())
 
-api.createThread(target=recvMethod)  # 子线程 阻塞接收消息
-# api.createThread(target=inputMethod) # 子线程 阻塞等待输入
+
+client=socket_client('192.168.2.121')
+print(f"Client > Connected.")
+
+createThread(target=recvMethod)  # 子线程 阻塞接收消息
+createThread(target=inputMethod) # 子线程 阻塞等待输入
--- server.py
+++ server.py
@@ -1,17 +1,30 @@
 from api import *
 
-server=socket_server('192.168.2.121',8000)
+server=socket_server('192.168.2.121')
 
 
 class Client:
     def __init__(self,socket_client) -> None:
         self.conn,self.address=socket_client
 
+
+
+clients=[]
+def clientThread():
+    while True:
+        client=Client(server.accept())
+        if client not in clients:
+            clients.append(client)
+            print(f"{client.address} > connected.")
+createThread(clientThread)
+
 def broadcast():
     while True:
         data=input()
         for client in clients:
             send(client.conn,f'Server > {data}')
+createThread(broadcast)
+
 def receive():
     while True:
         for client in clients:
@@ -19,15 +32,6 @@ def receive():
             print(data)
             if data is not None:
                 print(f'{client.address} > {data}')
-
-def clientThread():
-    while True:
-        client=Client(server.accept())
-        if client not in clients:
-            clients.append(client)
-
-clients=[]
-createThread(clientThread)
-createThread(broadcast)
+createThread(receive)
 
 
